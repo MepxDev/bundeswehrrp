ad3dbd74cc535c561abbc425aa99cefaf941fcf3
MepxDev <136330802+MepxDev@users.noreply.github.com>
2025-04-14 12:06:32 +0200

    Update and rename scripts.js to script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('languageSelect').addEventListener('change', function () {
+  const selected = this.value;
+  alert('Language switching not fully implemented yet.\nSelected: ' + selected);
+});
